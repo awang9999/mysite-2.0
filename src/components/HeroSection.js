ce2aff7e7b65f9bb10461c49a57b374afd8dbe26
aw576 <aw576@cornell.edu>
2022-07-08 00:02:10 -0700

    added extra content div for HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,27 +7,17 @@ import './HeroSection.css';
 
 function HeroSection() {
 
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-  // We listen to the resize event
-  window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
-
   return (
     <>
+      <video src='/videos/website_hero.mp4' autoPlay loop muted playsinline />
       <div className='hero-container'>
-        <video src='/videos/website_hero.mp4' autoPlay loop muted playsinline />
-        <h3>Hello! I'm</h3>
-        <h1>Alexander</h1>
-        <Link to="/#about" className='more-chevron'>
-          <i className="fa-solid fa-chevron-down"></i>
-        </Link>
+        <div className='hero-content'>
+          <h3>Hello! I'm</h3>
+          <h1>Alexander</h1>
+          <Link to="/#about" className='more-chevron'>
+            <i className="fa-solid fa-chevron-down"></i>
+          </Link>
+        </div>
       </div>
     </>
   );
