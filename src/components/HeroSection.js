28d771555937b4f44b92d1a2fed41385aa344246
aw576 <aw576@cornell.edu>
2022-07-07 22:22:51 -0700

    maybe fix for viewport height bug in mobile

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import { Element } from 'react-scroll';
-
 import '../App.css';
 import './HeroSection.css';
 
 function HeroSection() {
+
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
   return (
-    <Element id='top' name='top'>
+    <>
       <div className='hero-container'>
         <video src='/videos/website_hero.mp4' autoPlay loop muted playsinline />
         <h3>Hello! I'm</h3>
@@ -18,7 +29,7 @@ function HeroSection() {
           <i className="fa-solid fa-chevron-down"></i>
         </Link>
       </div>
-    </Element>
+    </>
   );
 }
 
