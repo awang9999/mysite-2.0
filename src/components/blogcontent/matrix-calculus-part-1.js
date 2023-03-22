4adedaaad4eb78260f02165b2487c7176c66be92
Alexander Wang <alexander.wang2001@gmail.com>
2023-03-22 14:08:01 -0400

    changed matrix animation to still image

--- src/components/blogcontent/matrix-calculus-part-1.js
+++ src/components/blogcontent/matrix-calculus-part-1.js
@@ -11,66 +11,7 @@ import MatrixCalculusTOC from './matrix-calculus-toc.js';
 function MatrixCalculusPart1() {
     
     const articleSource = "blog/matrix-calculus-chapter1.md";
-
-    React.useEffect(() => {
-
-        var canvas, ctx;
-
-        var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ' +
-            'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ' +
-            'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
-  
-        letters = letters.split('');
-    
-        // Setting up the columns
-        var fontSize = 15;
-        var columns;
-
         
-        // Setting up the drops
-        var drops = [];
-
-        // Setting up the draw function
-        function draw() {
-            ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-            for (var i = 0; i < drops.length; i++) {
-                var text = letters[Math.floor(Math.random() * letters.length)];
-                
-                colorText(text, i * fontSize, drops[i] * fontSize, '#0f0', fontSize + "px");
-
-                drops[i]++;
-
-                if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
-                    drops[i] = 0;
-                }
-            }
-        }
-
-        function colorText(showWords, textX, textY, fillColor, fontSize) {
-            ctx.font = `${fontSize} Courier New`;
-            ctx.fillStyle = fillColor;
-            ctx.fillText(showWords, textX, textY);
-        }
-        
-        canvas = document.querySelector('canvas');
-        ctx = canvas.getContext('2d');
-
-        columns = canvas.width / fontSize;
-
-        const compStyles = window.getComputedStyle(canvas);
-        canvas.width = compStyles.width.substr(0, compStyles.width.length - 2);
-        canvas.height = compStyles.height.substr(0, compStyles.height.length - 2);
-
-        for (var i = 0; i < columns; i++) {
-            drops[i] = 1;
-        }
-
-        setInterval(draw, 70);
-        
-    }, []);
-    
     return (
         <>
             <ScrollToTop>
@@ -87,7 +28,7 @@ function MatrixCalculusPart1() {
                         <hr />
 
                         <div className='blog-article-hero'>
-                            <canvas></canvas>
+                            <img src='./images/matrix-background.jpg' alt='matrix text decoration'/>
                         </div>
 
                         <MatrixCalculusTOC />
--- src/components/blogcontent/matrix-calculus-preface.js
+++ src/components/blogcontent/matrix-calculus-preface.js
@@ -11,65 +11,6 @@ import MatrixCalculusTOC from './matrix-calculus-toc.js'
 function MatrixCalculusPreface() {
     
     const articleSource = "blog/matrix-calculus-preface.md";
-
-    React.useEffect(() => {
-
-        var canvas, ctx;
-
-        var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ' +
-            'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ' +
-            'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
-  
-        letters = letters.split('');
-    
-        // Setting up the columns
-        var fontSize = 15;
-        var columns;
-
-        
-        // Setting up the drops
-        var drops = [];
-
-        // Setting up the draw function
-        function draw() {
-            ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-            for (var i = 0; i < drops.length; i++) {
-                var text = letters[Math.floor(Math.random() * letters.length)];
-                
-                colorText(text, i * fontSize, drops[i] * fontSize, '#0f0', fontSize + "px");
-
-                drops[i]++;
-
-                if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
-                    drops[i] = 0;
-                }
-            }
-        }
-
-        function colorText(showWords, textX, textY, fillColor, fontSize) {
-            ctx.font = `${fontSize} Courier New`;
-            ctx.fillStyle = fillColor;
-            ctx.fillText(showWords, textX, textY);
-        }
-        
-        canvas = document.querySelector('canvas');
-        ctx = canvas.getContext('2d');
-
-        columns = canvas.width / fontSize;
-
-        const compStyles = window.getComputedStyle(canvas);
-        canvas.width = compStyles.width.substr(0, compStyles.width.length - 2);
-        canvas.height = compStyles.height.substr(0, compStyles.height.length - 2);
-
-        for (var i = 0; i < columns; i++) {
-            drops[i] = 1;
-        }
-
-        setInterval(draw, 70);
-        
-    }, []);
     
     return (
         <>
@@ -86,7 +27,7 @@ function MatrixCalculusPreface() {
                         <hr />
 
                         <div className='blog-article-hero'>
-                            <canvas></canvas>
+                            <img src='./images/matrix-background.jpg' alt='matrix text decoration'/>
                         </div>
 
                         <MatrixCalculusTOC />
